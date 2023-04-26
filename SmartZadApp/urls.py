59ebc9a7a1db04bf7e3ac8e53162835aa2374d59
faroukelgazzar <faroukelgazzar@outlook.com>
2023-04-27 01:09:48 +0200

    Render1

--- SmartZadApp/urls.py
+++ SmartZadApp/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Dashboard import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.index, name='index'),
     path('admin/', admin.site.urls),
 ],
+
+urlpatterns += staticfiles_urlpatterns()
