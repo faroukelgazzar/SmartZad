b4e1428f4068edc50391474d4a32df1f5bf0b525
faroukelgazzar <faroukelgazzar@outlook.com>
2023-04-27 12:38:36 +0200

    Render11

--- SmartZadApp/settings.py
+++ SmartZadApp/settings.py
@@ -32,7 +32,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, "media")
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-)y^tg-m2q_s*=mamh_438p=-!=6qb#6sa=2lg^e_%rb9wm@a+=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
