e2e06a8a78278e7a626a237dd5deed76d03d70aa
faroukelgazzar <faroukelgazzar@outlook.com>
2023-04-27 14:45:14 +0200

    Render17

--- build_files.sh
+++ build_files.sh
@@ -2,11 +2,9 @@
 #python manage.py collectstatic
 #pip list --format=freeze > requirements.txt
 
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+
 
 #poetry install
-pip list --format=freeze > requirements.txt
+#pip list --format=freeze > requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
