771b353aa67cee2a40971d0fb20394b4e4c743cc
faroukelgazzar <faroukelgazzar@outlook.com>
2023-04-29 12:24:34 +0300

    Renderenv1

--- SmartZadApp/settings.py
+++ SmartZadApp/settings.py
@@ -161,7 +161,7 @@ STATICFILES_DIRS = [
 # in your application directory on Render.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 WHITENOISE_AUTOREFRESH = True
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 
