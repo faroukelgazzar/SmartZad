2ddbdb8c455b5bcc07d7fd48f5496fec4c79d780
faroukelgazzar <faroukelgazzar@outlook.com>
2023-08-10 01:19:04 +0300

    AWS EC2

--- SmartZadApp/settings.py
+++ SmartZadApp/settings.py
@@ -190,3 +190,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'SystemUsers/login.html'
 LOGOUT_URL = 'SystemUsers/login.html'
+
+
+# AWS S3 Setting
+AWS_ACCESS_KEY_ID = 'AKIAWMYVZ5E74TCTWAA2'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'zads3'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
